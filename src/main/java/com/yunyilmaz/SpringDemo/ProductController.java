017404380f1981961a9143e4fe08726064dd67b4
Yakup Unyilmaz <yakupunyilmaz@gmail.com>
2020-03-05 13:26:34 +0300

    Add ProductService

--- src/main/java/com/yunyilmaz/SpringDemo/ProductController.java
+++ src/main/java/com/yunyilmaz/SpringDemo/ProductController.java
@@ -1,6 +1,7 @@
 package com.yunyilmaz.SpringDemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,15 @@ public class ProductController {
     @GetMapping("/ping")
     public String ping() {
         return "Ping";
+    }
+
+    @GetMapping("/{productId}")
+    public ProductDTO readProductById (@PathVariable("productId") long id){
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setId(id);
+        productDTO.setName("Product" + 1);
+        productDTO.setPrice((int) (id * 10.5));
+
+        return productDTO;
     }
 }
